201481d3b080d7a80f3b30c0f5795dc45e26bd6e
Richard Wales <richard_wales@knights.ucf.edu>
2016-10-15 22:25:38 -0400

    Updated FileController to use json

--- FileController.py
+++ FileController.py
@@ -1,7 +1,40 @@
+import json
+import os
+
 class FileController:
-    def __init__(self, filename):
-        self.filename = filename
-    
+    def __init__(self):
+        self.filename = 'links.json'
+        self.links = {}
+
+    def process_json(self):
+        """
+        Processes the json file as a python dictionary for O(1) lookup
+        """
+        if os.path.isfile(self.filename):
+            links_file = open(self.filename).read()
+            self.links = json.loads(links_file)
+        else:
+            self.links = {}
+
+    def print_uploads(self):
+        """
+        Prints each video title that has been uploaded 
+        """
+        print 'Uploaded videos:'
+        for title in self.links:
+            print title
+
+    def return_link(self, title):
+        """
+        Returns the link that is mapped from the passed in title
+        Return None if the title is NOT in the dictionary
+        Args:
+            title: a string that represents the desired video title
+        """
+        if title in self.links:
+            return 'https://www.youtube.com/watch?' + self.links[title]
+        return None
+
     def write_link(self, name, link):
         """
         Writes a video filename and link to the file associated
@@ -11,7 +44,8 @@ class FileController:
             name: A string representing the file name
             link: A string representing the youtube link
         """
-        with open(self.filename, 'a') as file:
-            file.write(name + '-' + link)
+        self.links[name] = link
+        with open(self.filename, 'w') as fp:
+            json.dump(self.links, fp)
         
     
